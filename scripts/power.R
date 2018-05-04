3c88fa0117d8093a7dab52e4258285a7d0d46a13
Aaron Peikert <aaron.peikert@posteo.de>
2018-05-04 22:46:36 +0200

    add rmd stub

--- scripts/power.R
+++ scripts/power.R
@@ -15,29 +15,8 @@ df <- mutate(df, power = map(files, calc_power, d = .2))
 #----unnest----
 df_unnest <- unnest(df)
 
-#----plot-power----
-df_unnest %>% ggplot(aes(time, power)) +
-  geom_violin() +
-  facet_wrap(~event, ncol = 2) +
-  theme_minimal()
-
 #----calc-errors----
 df_unnest <- mutate(df_unnest,
                     closest_error = map_int(power, which_near, errors$power),
                     typeS = errors$typeS[closest_error],
                     typeM = errors$typeM[closest_error])
-
-#----plot-typem----
-df_unnest %>% ggplot(aes(time, typeM)) +
-  geom_violin() +
-  facet_wrap(~event) +
-  theme_minimal()
-
-#----plot-typem-d----
-df_unnest %>% mutate(d = abs(d)) %>% 
-  ggplot(aes(d, typeM, color = event)) + geom_point() + theme_minimal()
-
-#----table-typem----
-df_unnest %>% group_by(event, time) %>% summarise(typeM = mean(typeM)) %>% 
-  pander::pander()
-
