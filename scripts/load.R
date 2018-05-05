d432b3511c70ca9ed68184ed3b4643d7288dc854
aaronpeikert <aaron.peikert@posteo.de>
2018-05-05 12:24:46 +0200

    add id per study

--- scripts/load.R
+++ scripts/load.R
@@ -1,9 +1,9 @@
 if(!require("pacman"))install.packages("pacman")
-pacman::p_load("here", "tidyverse")
+pacman::p_load("here", "tidyverse", "digest")
 #----load----
 file_names <- list.files(here("data"), "\\.csv$")
 df <- tibble(files = map(here("data", file_names), read_csv),
              event = str_split_fixed(file_names, "-", n = 2)[, 1],
              time = str_split_fixed(file_names, "-", n = 2)[, 2] %>%
                str_replace("\\.csv", ""))
-
+df <- mutate(df, files = files %>% map(~cbind(.x, id = .x %>% rowwise() %>% do(id = digest(.)) %>% unlist()) %>% mutate(id = as.character(id))))
