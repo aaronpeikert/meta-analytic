66210d6caed1d4bd3f8dfd945df0a7e03fa2e582
aaronpeikert <aaron.peikert@posteo.de>
2018-05-05 12:26:42 +0200

    add animated plot

--- scripts/power.R
+++ scripts/power.R
@@ -3,7 +3,7 @@ pacman::p_load("here", "tidyverse", "pwr")
 source(here("scripts", "load.R"))
 source(here("scripts", "retrodesign.R"))
 
-#----calc-power----
+#----calc-power-1----
 calc_power <- function(df, d){
   df %>% pull("N") %>%
     map(~pwr.t.test(.x, d = d, type = "paired")) %>% 
@@ -20,3 +20,14 @@ df_unnest <- mutate(df_unnest,
                     closest_error = map_int(power, which_near, errors$power),
                     typeS = errors$typeS[closest_error],
                     typeM = errors$typeM[closest_error])
+
+#----calc-power-2----
+d_assumed <- seq(.1, .7, .01)
+df_ds <- d_assumed %>%
+  map(~mutate(df, power = map(files, calc_power, d = .x))) %>% 
+  map(unnest) %>%
+  map(~mutate(.x, closest_error = map_int(power, which_near, errors$power),
+                         typeS = errors$typeS[closest_error],
+                         typeM = errors$typeM[closest_error])) %>%
+  map2(d_assumed, ~mutate(.x, d_assumed = .y)) %>% 
+  bind_rows()
